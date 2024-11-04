9552f831723642fb0a2922a9c73d6acc0eac2c36
Yoel <yobelb8@gmail.com>
2024-11-04 13:55:46 +0000

    Al día

--- app/src/main/java/com/yoel/corroutineszerotohero/MainActivity.kt
+++ app/src/main/java/com/yoel/corroutineszerotohero/MainActivity.kt
@@ -10,18 +10,30 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import com.yoel.corroutineszerotohero.ui.theme.CorroutinesZeroToHeroTheme
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Response
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val retrofit = RetrofitHelper.getInstance()
+        lifecycleScope.launch() {
+            val resultado :Response<SuperHeroDataResponse> = retrofit.getSuperheroes("a")
+            withContext(Dispatchers.Main){
+                if (resultado.isSuccessful){
+                    Log.i("ejemplo", "${resultado}")
+                }
+            }
+        }
         setContent {
             CorroutinesZeroToHeroTheme {
                 Surface {
@@ -34,7 +46,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SuperHeroList() {
-    val retrofit = RetrofitHelper.getInstance()
     Text(
         text = "",
         modifier = Modifier
